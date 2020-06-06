f172c65162581458fab6c593843effbe17651a0c
fishguy6564 <37878245+fishguy6564@users.noreply.github.com>
2020-06-05 23:45:34 -0400

    Echo commands and large data fix
    - Fixes an issue where data larger than 4080 could not be read.
    - Added optional solution if using echoCommands

--- example_PokemonSwSh_SurpriseTrade.py
+++ example_PokemonSwSh_SurpriseTrade.py
@@ -41,20 +41,38 @@ global_dev = None
 global_out = None
 global_in = None
 
-#Sends string commands to the switch
-def sendCommand(content):
-    global_out.write(struct.pack("<I", (len(content)+2)))
-    global_out.write(content)
-
+#Hotfix to handle data received larger than 4080 bytes
+#Very ineficient, but it will get the job done for now
 def readData():
 	size = int(struct.unpack("<L", global_in.read(4, timeout=0).tobytes())[0])
 
-	#Converts received data to integer array
 	data = [0] * size
-	x = global_in.read(size, timeout=0).tobytes()
-	for i in range(size):
-		data[i] = int(x[i])
+	if size > 4080:
+		i = 0
+		while i < size:
+			chunkSize = 4080
+			if size - i < 4080:
+				chunkSize = size - i
+			x = global_in.read(chunkSize, timeout=0).tobytes()
+
+			for j in range(len(x)):
+				data[i] = x[j]
+				i+=1
+	else:
+		x = global_in.read(size, timeout=0).tobytes()
+
+		#Converts received data to integer array
+		for i in range(size):
+			data[i] = int(x[i])
+
 	return data
+
+#Sends string commands to the switch
+#Uncomment return if using echoCommands
+def sendCommand(content):
+    global_out.write(struct.pack("<I", (len(content)+2)))
+    global_out.write(content)
+    #return readData()
 
 #Using method from Goldleaf
 def connect_switch():
